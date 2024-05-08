b8569e8026be3398644ae47a6a7a36a06f449700
thiengoten <thiengoten6@gmail.com>
2024-05-09 01:03:22 +0700

    feat: 🎸 Add Login Page and some dependences

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+export default function AuthLayout({
+  children, // will be a page or nested layout
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <section className="flex min-h-screen items-center justify-center">
+      {children}
+      <div className="p-4 pt-2"></div>
+    </section>
+  )
+}
