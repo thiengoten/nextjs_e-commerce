dc4a4bd1012c617e1e19472725a4997d4fa79eca
thiengoten <thiengoten6@gmail.com>
2024-05-10 02:19:26 +0700

    Update Prettier configuration and login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -13,43 +13,43 @@ import { Label } from "@/components/ui/label"
 
 export default function Page() {
   return (
-    <Card className="mx-auto max-w-sm">
+    <Card className='mx-auto max-w-sm'>
       <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardTitle className='text-2xl'>Login</CardTitle>
         <CardDescription>
           Enter your email below to login to your account
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
+        <div className='grid gap-4'>
+          <div className='grid gap-2'>
+            <Label htmlFor='email'>Email</Label>
             <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
+              id='email'
+              type='email'
+              placeholder='m@example.com'
               required
             />
           </div>
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
-              <Link href="#" className="ml-auto inline-block text-sm underline">
+          <div className='grid gap-2'>
+            <div className='flex items-center'>
+              <Label htmlFor='password'>Password</Label>
+              <Link href='#' className='ml-auto inline-block text-sm underline'>
                 Forgot your password?
               </Link>
             </div>
-            <Input id="password" type="password" required />
+            <Input id='password' type='password' required />
           </div>
-          <Button type="submit" className="w-full">
+          <Button type='submit' className='w-full'>
             Login
           </Button>
-          <Button variant="outline" className="w-full">
+          <Button variant='outline' className='w-full'>
             Login with Google
           </Button>
         </div>
-        <div className="mt-4 text-center text-sm">
+        <div className='mt-4 text-center text-sm'>
           Don&apos;t have an account?{" "}
-          <Link href="#" className="underline">
+          <Link href='/register' className='underline'>
             Sign up
           </Link>
         </div>
